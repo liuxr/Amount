4144662612777449f598a3aaeba700fc6cf86088
liuxr <394523078@qq.com>
2016-11-02 17:13:28 +0800

    增加php服务请求

---
+++ MyPay/Crypt3Des.cs
@@ -0,0 +1,98 @@
+﻿/* 
+   ======================================================================== 
+    文 件 名：     
+	功能描述：                
+    作    者:	Cumin           
+    创建时间： 	2016/11/2 15:23:34
+	版    本:	V1.0.0
+   ------------------------------------------------------------------------
+	历史更新纪录
+   ------------------------------------------------------------------------
+	版    本：           修改时间：           修改人：          
+	修改内容：
+   ------------------------------------------------------------------------
+	Copyright (C) 2016   北京荣大科技有限公司
+   ======================================================================== 
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyPay
+{
+    public class Crypt3Des
+    {
+        private string key = string.Empty;
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+        public Crypt3Des(string key) {
+            this.key = key;
+        }
+        private SymmetricAlgorithm mCSP = new DESCryptoServiceProvider();
+        public string Encrypt(string input) {
+            int size = mCSP.BlockSize;
+            string input1 = Pkcs5_pad(input, size);
+            string k = key.PadRight(24, '0');
+            mCSP.Mode = CipherMode.ECB;
+            mCSP.Key = Convert.FromBase64String(key);
+            //mCSP.IV=Convert.FromBase64String()
+            return "";
+        }
+
+        /// <summary>
+        /// 一种补充算法
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="blockSize"></param>
+        /// <returns></returns>
+        public string Pkcs5_pad(string text, int blockSize) {
+            int pad = blockSize - (text.Length % blockSize);
+            byte[] array = new byte[1];
+            array[0] = (byte)(pad);
+            string ch = Convert.ToString(System.Text.Encoding.ASCII.GetString(array));
+            return text + Repeat(ch, pad);
+        }
+
+        /// <summary>
+        /// 字符串重复次数
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="multiplier"></param>
+        /// <returns></returns>
+        public string Repeat(string str, int multiplier) {
+            string result = str;
+            for (int i = 0; i < multiplier; i++) {
+                result += str;
+            }
+            return result;
+        }
+
+
+        private void Test() {
+            TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+            des.Mode = CipherMode.ECB;
+            des.Padding = PaddingMode.PKCS7;
+            byte[] buffer = Encoding.Default.GetBytes("明文");
+            MemoryStream stream = new MemoryStream();
+            byte[] key = Convert.FromBase64String("AQjP4U1aCnnybWsmHUQ7BVIxHyrnQ2AP");
+            CryptoStream encStream = new CryptoStream(stream, des.CreateEncryptor(key, null), CryptoStreamMode.Write);
+            encStream.Write(buffer, 0, buffer.Length);
+            encStream.FlushFinalBlock();
+            byte[] res = stream.ToArray();
+            Console.WriteLine("result:" + Convert.ToBase64String(res));
+        }
+    }
+}
---
+++ MyPay/Request.cs
@@ -0,0 +1,53 @@
+﻿/* 
+   ======================================================================== 
+    文 件 名：     
+	功能描述：                
+    作    者:	Cumin           
+    创建时间： 	2016/11/2 15:03:52
+	版    本:	V1.0.0
+   ------------------------------------------------------------------------
+	历史更新纪录
+   ------------------------------------------------------------------------
+	版    本：           修改时间：           修改人：          
+	修改内容：
+   ------------------------------------------------------------------------
+	Copyright (C) 2016   北京荣大科技有限公司
+   ======================================================================== 
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace MyPay
+{
+    public class Request
+    {
+        private string Key = "keyjingcai2l.8ke520lhJin.Cai@ss283.229808e";
+
+        ///<summary>
+        ///采用https协议访问网络
+        ///</summary>
+        ///<param name="URL">url地址</param>
+        ///<param name="strPostdata">发送的数据</param>
+        ///<returns></returns>
+        public static string OpenReadWithHttps(string URL, string strPostdata, string strEncoding)
+        {
+            Encoding encoding = Encoding.Default;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "post";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.ContentType = "application/x-www-form-urlencoded";
+            byte[] buffer = encoding.GetBytes(strPostdata);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.GetEncoding(strEncoding)))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
